cd416aa2f5b12a0995c9f66f1d9cb33b9d3e9222
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2023-03-23 21:19:45 +0200

    Add files via upload

---
+++ EventmiApp/Eventmi/Controllers/EventController.cs
@@ -0,0 +1,170 @@
+﻿using Eventmi.Core.Contracts;
+using Eventmi.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventmi.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly IEventService eventService;
+
+        private readonly ILogger logger;
+
+        public EventController(IEventService _eventService, ILogger<EventController> _logger)
+        {
+            eventService = _eventService;
+            logger = _logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable <EventModel> eventModels = Enumerable.Empty<EventModel>();
+            try
+            {
+                eventModels = await eventService.GetAllAsync();
+            }
+            catch (Exception ex)
+            {
+
+
+                logger.LogError("EventController/Index", ex);
+                ViewBag.ErrorMessage = "Възникна непредвидена грешка!"; 
+                //throw new ArgumentException("Възникна непредвидена грешка!");
+            }
+
+            return View("All", eventModels);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var model = new EventModel()
+            { 
+                Start= DateTime.Today,
+                End= DateTime.Today
+            };
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(EventModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                await eventService.AddAsync(model);
+
+            }
+            catch (Exception ex )
+            {
+                logger.LogError("EventController/Add", ex);
+                ViewBag.ErrorMessage = "Възникна непредвидена грешка!";
+            }
+
+            return RedirectToAction(nameof(Index));   
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            EventModel model;
+
+            try
+            {
+                model = await eventService.GetEventAsync(id);
+                return View(model);
+            }
+            catch (ArgumentException aex)
+            {
+
+                ViewBag.ErrorMessage = aex.Message;
+            }
+            catch(Exception ex)
+            {
+                logger.LogError("EventController/Details", ex);
+
+                ViewBag.ErrorMessage = "Възникна непредвидена грешка!";
+            }
+
+            return RedirectToAction(nameof(model));
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            try
+            {
+                await eventService.DeleteAsync(id);
+            }
+            catch (Exception ex)
+            {
+
+                logger.LogError("EventController/Delete", ex);
+                ViewBag.ErrorMessage = "Възникна непредвидена грешка!";
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            EventModel model;
+
+            try
+            {
+                model = await eventService.GetEventAsync(id);
+                return View(model);
+            }
+            catch (ArgumentException aex)
+            {
+                ViewBag.ErrorMessage = aex.Message;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("EventController/Details", ex);
+                ViewBag.ErrorMessage = "Възникна непредвидена грешка!";
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EventModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                  return View(model);                
+            }
+
+            try
+            {
+                await eventService.UpdateAsync(model);
+                return RedirectToAction(nameof(Details), new { id = model.Id });
+            }
+            catch (ArgumentException aex)
+            {
+                ViewBag.ErrorMessage = aex.Message;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("EventLogger/Details", ex);
+                ViewBag.ErrorMessage = "Възникна непредвидена грешка!";
+            }
+
+            return RedirectToAction(nameof(Index));    
+        }
+    }
+}
